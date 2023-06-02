55cd6658e9103c3bad758eb363878df17835d540
laugharne <franck@maussand.net>
2023-06-02 15:08:03 +0200

    Revert test unit @ 1st time; compil+test successfull

--- test/TestVoting.js
+++ test/TestVoting.js
@@ -4,11 +4,37 @@ const { expect } = require('chai');
 
 
 contract("Voting", accounts => {
+	const _owner  = accounts[0];
+	const _voter1 = accounts[1];
+	const _voter2 = accounts[2];
+	const _voter3 = accounts[3];
+	const _voter4 = accounts[4];
 
-	let MyInstance;
+	let voting;
 
 	beforeEach(async function(){
-		MyInstance= await Voting.new({from: _owner});
+		voting= await Voting.new({from: _owner});
 	});
 
+	it("At first, no voter, no proposal", async () => {
+		await expectRevert(
+			voting.getVoter(_owner),
+			"You're not a voter"
+		);
+		await expectRevert(
+			voting.getVoter(_voter1),
+			"You're not a voter"
+		);
+		await expectRevert(
+			voting.getVoter(_voter2),
+			"You're not a voter"
+		);
+
+		await expectRevert(
+			voting.getOneProposal(0),
+			"You're not a voter"
+		);
+	});
+
+	
 });
